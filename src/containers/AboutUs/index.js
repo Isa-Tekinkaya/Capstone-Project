17fad9a2b354c439bb05bd8d04f5efcf2cebea24
Isa <isatekinkaya@yahoo.de>
2023-09-10 14:25:17 +0300

    deploy git commit -m deploy, git commit -m deploy

---
+++ src/containers/AboutUs/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import AboutUsHeader from "../../components/AboutUsHeader";
+import AboutUsContent from "../../components/AboutUsContent";
+
+const AboutUs = () => {
+  return (
+    <div style={{ minHeight: "85.3%" }}>
+      <AboutUsHeader />
+      <AboutUsContent />
+    </div>
+  );
+};
+
+export default AboutUs;
