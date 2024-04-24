e7423522a274080fe64bef8acc86931319e09212
Isa <isatekinkaya@yahoo.de>
2024-04-24 23:02:37 +0300

    Updates

---
+++ precache-manifest.766403f96ff88432cf81013087631dae.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3f4914a528b72e190a822ca34888107",
+    "url": "/Capstone-Project/index.html"
+  },
+  {
+    "revision": "4addc46911d5c1801f40",
+    "url": "/Capstone-Project/static/css/2.97d407c5.chunk.css"
+  },
+  {
+    "revision": "8366e74c7ccd32f0f643",
+    "url": "/Capstone-Project/static/css/main.65b4659f.chunk.css"
+  },
+  {
+    "revision": "4addc46911d5c1801f40",
+    "url": "/Capstone-Project/static/js/2.bc81db3d.chunk.js"
+  },
+  {
+    "revision": "1b4730aef7a0b0be157af031275f5f47",
+    "url": "/Capstone-Project/static/js/2.bc81db3d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8366e74c7ccd32f0f643",
+    "url": "/Capstone-Project/static/js/main.a0b6496f.chunk.js"
+  },
+  {
+    "revision": "2637167781bd63cda0c3",
+    "url": "/Capstone-Project/static/js/runtime-main.4b33e8ae.js"
+  },
+  {
+    "revision": "875fcfd932833ec79706485c1a1bd77b",
+    "url": "/Capstone-Project/static/media/Kaaba.875fcfd9.jpeg"
+  },
+  {
+    "revision": "fa2d540d2c346047ff5a911321569d1d",
+    "url": "/Capstone-Project/static/media/aboutimage.fa2d540d.png"
+  },
+  {
+    "revision": "3e26049d9396f49912688a986b4027b8",
+    "url": "/Capstone-Project/static/media/googleLogo.3e26049d.png"
+  },
+  {
+    "revision": "ef3a8c6f716b8b54beceebd934a37ffa",
+    "url": "/Capstone-Project/static/media/logo.ef3a8c6f.png"
+  },
+  {
+    "revision": "3523d2fb78d4b5c8a8dc48c64d169b82",
+    "url": "/Capstone-Project/static/media/sharon.3523d2fb.png"
+  },
+  {
+    "revision": "3c5d02b3676fe9ab54411383c8b6aa95",
+    "url": "/Capstone-Project/static/media/whyDonate.3c5d02b3.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.544043c0d2a0833a01ec0ecec9ac12da.js"
+  "/Capstone-Project/precache-manifest.766403f96ff88432cf81013087631dae.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Capstone-Project/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
