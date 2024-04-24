24f0ee5f9a65db1595607f1bcbde0f2ae5f6f815
Isa <isatekinkaya@yahoo.de>
2024-04-24 22:35:23 +0300

    Updates

---
+++ precache-manifest.a169e3631b0992cd964ef34f0f15a9f5.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "760c3ca39ade35c755d8a1fdc5a4d01e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3859b9951caeae79b578",
+    "url": "/static/css/2.97d407c5.chunk.css"
+  },
+  {
+    "revision": "edbc9ae291b01cca8286",
+    "url": "/static/css/main.242dadad.chunk.css"
+  },
+  {
+    "revision": "3859b9951caeae79b578",
+    "url": "/static/js/2.beab1180.chunk.js"
+  },
+  {
+    "revision": "1b4730aef7a0b0be157af031275f5f47",
+    "url": "/static/js/2.beab1180.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "edbc9ae291b01cca8286",
+    "url": "/static/js/main.c53da999.chunk.js"
+  },
+  {
+    "revision": "5a31f03637e996fb1d48",
+    "url": "/static/js/runtime-main.a48f2cd1.js"
+  },
+  {
+    "revision": "875fcfd932833ec79706485c1a1bd77b",
+    "url": "/static/media/Kaaba.875fcfd9.jpeg"
+  },
+  {
+    "revision": "fa2d540d2c346047ff5a911321569d1d",
+    "url": "/static/media/aboutimage.fa2d540d.png"
+  },
+  {
+    "revision": "3e26049d9396f49912688a986b4027b8",
+    "url": "/static/media/googleLogo.3e26049d.png"
+  },
+  {
+    "revision": "ef3a8c6f716b8b54beceebd934a37ffa",
+    "url": "/static/media/logo.ef3a8c6f.png"
+  },
+  {
+    "revision": "3523d2fb78d4b5c8a8dc48c64d169b82",
+    "url": "/static/media/sharon.3523d2fb.png"
+  },
+  {
+    "revision": "3c5d02b3676fe9ab54411383c8b6aa95",
+    "url": "/static/media/whyDonate.3c5d02b3.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Capstone-Project/precache-manifest.c7f1003a2cf83f84b2b10e0547b57730.js"
+  "/precache-manifest.a169e3631b0992cd964ef34f0f15a9f5.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Capstone-Project/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
