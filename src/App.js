a8fd373a4ccc2b89c6942e2531187b76d42535d5
Isa Tekinkaya <isatekinkaya@yahoo.de>
2024-06-15 18:42:08 +0300

    home

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function App() {
     <div className="App">
       <NavBar />
       <Switch>
-        <Route exact path="/" component={Home} />
+        <Route exact path="/Home" component={Home} />
         <Route path="/About" component={About} />
         <Route path="/Blog" component={Blog} />
         <Route path="/Contact_us" component={ContactUs} />
